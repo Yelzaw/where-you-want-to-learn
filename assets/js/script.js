325313f2fffca6892e39de425fb27fc28526c63c
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2022-11-29 07:02:31 -0400

    added mini google map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-console.log('connected')
+
+// set temp location Fredericton
+var fredericton = {lat: 45.9454, lng: -66.6656};
+
+// function to call the map with user input data
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: fredericton,
+    zoom: 10
+  });
+// set marker on the map
+  const marker = new google.maps.Marker({
+    position: fredericton,
+    map: map,
+  });
+
+}
+
+// initMap();
