81cb90abae7d12a9c83f2e742ab3c8439b638dae
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2022-11-30 12:28:07 -0400

    added comparison currency

--- script.js
+++ script.js
@@ -21,26 +21,34 @@ $(document).ready(function(){
 
      $('#currency')
 
-
+     // CURRENCY EXCHANGE RATE
      function currecncyExchangeRate(input){
      //call restcountries.com api
           //get country data to get currency code and currency name
-          $("#today-rate").siblings("h6").text("Current Exchange Rate");
+          $("#today-rate").siblings("h6").text("Today Exchange Rate");
           var exchangeInfo = document.querySelector('#today-rate');
           exchangeInfo.innerHTML="";
 
           var currencyCode = "";
           var currencyName = "";
-          var country= "https://restcountries.com/v3.1/name/"+input+"?fullText=true";
+          var compareCurrency = "CAD";
+          var compareName = "Canadian dollar";
+
+          if (input=="canada"){
+               compareCurrency = "USD";
+               compareName = "United State dollar";
+          }
+
+          var country= "https://restcountries.com/v3.1/name/"+input+"?fullText=true"; //API link to get country information
                fetch(country)
                     .then(function(response){
                          if(response.ok){
                               response.json().then(function(data){
-                                   console.log(data);//JSON data to show in console
+                                   // console.log(data);//JSON data to show in console
                                    currencyCode = (Object.keys(data[0].currencies))[0];
-                                   console.log(currencyCode);
+                                   // console.log(currencyCode);
                                    currencyName = (Object.values(data[0].currencies))[0].name;
-                                   console.log(currencyName);
+                                   // console.log(currencyName);
                                    currency()
                               })
                          } else {
@@ -62,28 +70,28 @@ $(document).ready(function(){
                     redirect: 'follow',
                     headers: myHeaders
                     };
-
-               fetch("https://api.apilayer.com/exchangerates_data/convert?to="+"usd"+"&from="+currencyCode+"&amount=1", requestOptions)
+                    //Another API to get exchange information
+               fetch("https://api.apilayer.com/exchangerates_data/convert?to="+compareCurrency+"&from="+currencyCode+"&amount=1", requestOptions) 
                .then(function(response){
                     if(response.ok){
                          response.json().then(function(data){
-                              console.log(data);
+                              // console.log(data);
                               var resultLocalUsd = data.result;
-                              console.log(resultLocalUsd);
+                              // console.log(resultLocalUsd);
                               var showResult = document.createElement("p");
-                              showResult.textContent="1 "+currencyName+" = "+resultLocalUsd+" US Dollars";
+                              showResult.textContent="1 "+currencyName+" = "+resultLocalUsd+" "+compareName;
                               exchangeInfo.appendChild(showResult);
                          })
                     }
-                    fetch("https://api.apilayer.com/exchangerates_data/convert?to="+currencyCode+"&from="+"usd"+"&amount=1", requestOptions)
+                    fetch("https://api.apilayer.com/exchangerates_data/convert?to="+currencyCode+"&from="+compareCurrency+"&amount=1", requestOptions)
                     .then(function(response){
                     if(response.ok){
                          response.json().then(function(data){
-                              console.log(data);
+                              // console.log(data);
                               var resultUsdLocal = data.result;
-                              console.log(resultUsdLocal);
+                              // console.log(resultUsdLocal);
                               var showResult = document.createElement("p");
-                              showResult.textContent="1 US Dollars = "+resultUsdLocal+" "+currencyName;
+                              showResult.textContent="1 "+compareName+" = "+resultUsdLocal+" "+currencyName;
                               exchangeInfo.appendChild(showResult);
                          })
                     }
