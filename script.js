5a7049e18c72945717b034dbe5875dabe54d1973
Valerii Kryshtal <112430151+ValeriiKryshtal@users.noreply.github.com>
2022-11-29 21:17:00 -0400

    updated basic HTML structure, added listener to the button

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('connected')
+$(document).ready(function(){
+     console.log('connected')
+     $('#head')
+     $('#head-container')
+     $('#calendar-holder')
+     $('#head-tittle')
+     $('#main-container')
+     $('#search')
+     $('#submit-btn').on('click', function(){
+          console.log('yeah!!')   
+          const input = $('#input')
+          const inputValue = input[0].value
+          console.log(inputValue) 
+          function map(){
+
+          }
+          function wikki(){
+
+          }
+          function currency(){
+               
+          }
+     })
+     $('#content')
+     $('#city name')
+     $('#history')
+     $('#map')
+     $('#currency')
+
+})
